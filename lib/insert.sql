dcbeab8250562f8843c98139b0d63fa305e9379e
Precious Humphrey <phumphrey23@gmail.com>
2018-10-02 00:00:37 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
+  ('Help me buy a guitar', 'music', 500.00, '2018-06-30', '2018-07-30'),
+  ('Drawing lessons', 'art', 400.00, '2018-07-03', '2018-08-30'),
+  ('Buy a new amplifier', 'music', 250.00, '2018-08-31', '2018-09-15'),
+  ('Animal shelter needs dog food', 'charity', 300.00, '2018-03-14', '2018-06-30'),
+  ('Replacement paint', 'art', 50.00, '2018-04-12', '2018-04-25'),
+  ('Toys for annual holiday drive', 'charity', 1500.00, '2018-09-01', '2018-11-30'),
+  ('Fix sink in the kitchen', 'plumbing', 250.00, '2018-01-27', '2018-02-18'),
+  ('Canvas for watercolor', 'art', 25.00, '2018-06-04', '2018-06-09'),
+  ('Gone Girl book', 'books', 12.50, '2018-11-27', '2018-12-20'),
+  ('Bathroom shower repairs', 'plumbing', 179.00, '2018-05-16', '2018-05-18');
+
+INSERT INTO users ()
